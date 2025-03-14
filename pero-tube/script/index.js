b4bd0a1ae96f11ddcd65a22fa4fa850a29719b26
nurulafsarinfo <mdnurulafsar123afsar@gmail.com>
2025-03-14 06:56:56 +0600

    added some feature and almost complete project

--- pero-tube/script/index.js
+++ pero-tube/script/index.js
@@ -31,6 +31,11 @@
 
 // loadCatagories();
 
+
+// show loader 
+
+
+
 function loadCategories (){
     fetch("https://openapi.programming-hero.com/api/phero-tube/categories")
     .then(res => res.json())
@@ -72,8 +77,8 @@ function displayAllCatag (allCategories){
 // }
 
 // Load videos
-function loadVideos(){
-    fetch(' https://openapi.programming-hero.com/api/phero-tube/videos')
+function loadVideos(searchText = ""){
+    fetch(`https://openapi.programming-hero.com/api/phero-tube/videos?title=${searchText}`)
     .then(response => response.json())
     .then(data => {
         removeActiveClass();
@@ -125,11 +130,13 @@ const displayVideos = (videos) => {
 
             <div class="intro">
                 <h2 class="text-sm font-semibold" >${video.title}</h2>
-                <p class="flex gap-2 text-sm text-gray-400">${video.authors[0].profile_name} <img class="w-5 h-5" src="https://img.icons8.com/?size=60&id=lalayI2ulYNt&format=png" alt=""></p>
+                <p class="flex gap-2 text-sm text-gray-400">${video.authors[0].profile_name} 
+                ${video.authors[0].verified ? `<img class="w-5 h-5" src="https://img.icons8.com/?size=60&id=lalayI2ulYNt&format=png" alt=""></p>` : ``}
 
                 <p class="text-sm text-gray-400">${video.others.views}</p>
             </div>
         </div>
+            <button onclick=loadVideoDetails('${video.video_id}') class="btn btn-block">Show Details</button>
         </div>
         `;
 
@@ -142,7 +149,7 @@ const displayVideos = (videos) => {
 // load Categories Videos
 
 const loadCategoriesVideos = (id) => {
-    const url = ` https://openapi.programming-hero.com/api/phero-tube/category/${id}`;
+    const url = `https://openapi.programming-hero.com/api/phero-tube/category/${id}`;
     console.log(url);
 
     fetch(url)
@@ -168,5 +175,56 @@ function removeActiveClass(){
     }
 
 }
+
+// load video Details
+const loadVideoDetails = (videoId) => {
+    console.log(videoId);
+    const url = `https://openapi.programming-hero.com/api/phero-tube/video/${videoId}`;
+
+    fetch(url)
+    .then(res => res.json())
+    .then(data => displayVideoDetails(data.video))
+}
+
+// display video contant 
+const displayVideoDetails = (video) => {
+    console.log(video);
+
+    // access the video data properly
+    
+    const { thumbnail, title, authors, others, description } = video;
+
+    document.getElementById("video_details").showModal();
+    const detailsContainer = document.getElementById("details_container");
+
+    detailsContainer.innerHTML = `
+    <div class="card bg-base-100  shadow-sm">
+    <figure>
+        <img
+        src="${thumbnail}" alt="video thumbnail" />
+     </figure>
+     <div class="card-body">
+        <h2 class="card-title text-xl">${title}</h2>
+        <p>${description}</p>
+        <div class="pt-5 flex justify-center items-center gap-3"> 
+            <div class="avatar">
+                <div class="ring-primary ring-offset-base-100 w-12 rounded-full ring ring-offset-2">
+                <img src="${video.authors[0].profile_picture}" />
+                </div>
+            </div>
+            <p class="text-xl">${video.authors[0].profile_name}</p>
+
+     </div>
+     </div>
+    </div>
+    `
+}
+
+
+document.getElementById("search-input").addEventListener("keyup", (e) => {
+    const input = e.target.value;
+    loadVideos(input);
+})
+
 
 loadCategories();
